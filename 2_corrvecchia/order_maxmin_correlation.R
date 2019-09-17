fb377ad7439d4003059722d185d4e4b1f8c5f7c5
Myeongjong Kang <myeongjong@stat.tamu.edu>
2019-09-17 11:51:43 -0500

    Create order_maxmin_correlation.R

---
+++ 2_corrvecchia/order_maxmin_correlation.R
@@ -0,0 +1,12 @@
+####################################################################
+####
+####  Author: Myeongjong (MJ) Kang (kmj.stat@gmail.com)
+####
+####  Description: 
+####
+####################################################################
+
+order_maxmin_correlation <- function(locs, covmodel, covparms) {
+  
+  
+}
