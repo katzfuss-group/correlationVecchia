a4c94d6ec35811b53c5047e59fa832b0aa7be368
myeongjong <kmj.stat@gmail.com>
2019-09-17 21:58:32 -0500

    [Build] Polish the code of the 'conditioning_nn()' function

--- 2_corrvecchia/corrvecchia.R
+++ 2_corrvecchia/corrvecchia.R
@@ -62,10 +62,8 @@ conditioning_nn <- function(m, dist.matrix)
   
   n     <- nrow(dist.matrix)
   NN    <- matrix(rep(NA, n^2), nrow = n, ncol = n) ; NN[1, 1] <- 1
-  for(i in 2:n){
-    k               <- min(i, m)
-    NN[i, seq(k)]   <- scanstatistics::dist_to_knn(dist.matrix[seq(i), seq(i)], k = min(i, m))[i, seq(k)]
-  }
+  
+  for(i in 2:n) NN[i, seq(k)] <- scanstatistics::dist_to_knn(dist.matrix[seq(i), seq(i)], k = min(i, m))[i, seq(k)]
   
   return(NN)
 }
