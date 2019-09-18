e598da3952fe19b507f3aab2143313956b173db2
myeongjong <kmj.stat@gmail.com>
2019-09-17 21:54:13 -0500

    [Build] Complete the 'conditioning_nn()' function

--- 2_corrvecchia/corrvecchia.R
+++ 2_corrvecchia/corrvecchia.R
@@ -38,7 +38,7 @@ corrvecchia_knownCovparms <- function(dat, locs, m, ordering = "maxmin", conditi
   locsord   <- locs[ord, , drop=FALSE]
   
   dist.matrix   <- distance_correlation(locsord, covmodel, covparms)
-  cond.sets     <- conditioning_nn(dist.matrix)
+  cond.sets     <- conditioning_nn(m, dist.matrix)
   
   # sparsity structure
   
@@ -57,15 +57,28 @@ distance_correlation <- function(locsord, covmodel, covparms)
   
 }
 
-conditioning_nn <- function(dist.matrix)
+conditioning_nn <- function(m, dist.matrix)
 {
   
   n     <- nrow(dist.matrix)
-  NN    <- matrix(rep(NA, n^2), nrow = n, ncol = n)
-  for(i in 1:n){
-    dist.matrix.nested <- dist.matrix[seq(i), seq(i)]
+  NN    <- matrix(rep(NA, n^2), nrow = n, ncol = n) ; NN[1, 1] <- 1
+  for(i in 2:n){
+    k                   <- min(i, m)
+    dist.matrix.nested  <- dist.matrix[seq(i), seq(i)]
+    NN[i, seq(k)]       <- scanstatistics::dist_to_knn(dist.matrix.nested, k = min(i, m))[i, seq(k)]
   }
+  
+  return(NN)
 }
+
+# # Test code for conditioning_nn()
+# locs <- matrix(runif(20, 0, 1), 10, 2)
+# dist.matrix <- as.matrix(dist(locs))
+# m <- 3
+# conditioning_nn(m, dist.matrix)
+
+
+
 
 ### Test validity of dist_to_knn() ###
 
