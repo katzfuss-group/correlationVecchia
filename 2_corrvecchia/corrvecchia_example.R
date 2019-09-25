af9340583fa51785f358ae9a82aa1065c8a1de1f
Myeongjong Kang <myeongjong@stat.tamu.edu>
2019-09-25 14:01:45 -0500

    [Test] Implement two toy examples to check code correctness

--- 2_corrvecchia/corrvecchia_example.R
+++ 2_corrvecchia/corrvecchia_example.R
@@ -11,6 +11,7 @@ rm(list = ls())
 
 library(GPvecchia)
 
+source("1_Pilot_Study/2_vecchia_specify_adjusted.R")
 source("2_corrvecchia/corrvecchia.R")
 source("2_corrvecchia/kldiv.R")
 
@@ -22,12 +23,15 @@ set.seed(12345)
 
 locs          <- matrix(runif(15^2 * 2, 0, 1), 15^2, 2)
 n             <- 15^2
-m             <- 15
+m             <- 10
+
 # covmodel
 cov.iso       <- function(locs, covparms) covparms[1] * exp(-fields::rdist(locs) / covparms[2])
 cov.aniso     <- function(locs, covparms) covparms[1] * exp(-fields::rdist(cbind(locs[ ,1] * covparms[3], locs[,2])) / covparms[2])
 covparms      <- c(1, 0.1, 1)
 
+locs.trans    <- cbind(locs[, 1] * covparms[3], locs[, 2])
+
 # true cov matrix
 Sigma <- cov.aniso(locs, covparms)
 
@@ -35,54 +39,54 @@ Sigma <- cov.aniso(locs, covparms)
 y <- as.numeric(t(chol(Sigma)) %*% rnorm(n))
 fields::quilt.plot(locs[,1], locs[,2], y)
 
-sim.iso     <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "euclidean", conditioning = "NN", covmodel = cov.iso, covparms = covparms)
 
-Sigma.ord       <- cov.aniso(sim.iso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.iso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.iso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
+### specify vecchia approximations
+approx <- list()
+
+# standard vecchia
+approx[[1]]           <- vecchia_specify_adjusted(locs, m, ordering = "maxmin", which.coord = NULL, cond.yz='y', conditioning = "NN")
+# pilot correlation-based vecchia
+approx[[2]]           <- vecchia_specify_adjusted(locs.trans, m, ordering = "maxmin", which.coord = NULL, cond.yz='y', conditioning = "NN")
+approx[[2]]$locsord   <- locs[approx[[2]]$ord,]
+# standard vecchia with the corrvecchia function
+approx[[3]]           <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "euclidean", conditioning = "NN", covmodel = cov.iso, covparms = covparms)
+# correlation-based vecchia with the corrvecchia function
+approx[[4]]           <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "correlation", initial.pt = NULL, conditioning = "NN", covmodel = cov.aniso, covparms = covparms)
+
+
+### compute approximate covariance matrices
+Sigma.hat   <- list()
+kls         <- c()
+for(i in 1:4){
+  
+  Sigma.ord       <- cov.aniso(approx[[i]]$locsord, covparms) # true cov in appropriate ordering
+  
+  U               <- createU(approx[[i]], c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
+  revord          <- order(approx[[i]]$ord)
+  Sigma.hat[[i]]  <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
+  
+  kls[i]          <- kldiv(Sigma, Sigma.hat[[i]])
+}
+
 kls
-
-sim.iso     <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "correlation", conditioning = "NN", covmodel = cov.iso, covparms = covparms)
-
-Sigma.ord       <- cov.aniso(sim.iso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.iso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.iso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
-kls
-
-sim.aniso   <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "euclidean", conditioning = "NN", covmodel = cov.aniso, covparms = covparms)
-
-Sigma.ord       <- cov.aniso(sim.aniso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.aniso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.aniso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
-kls
-
-sim.aniso   <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "correlation", conditioning = "NN", covmodel = cov.aniso, covparms = covparms)
-
-Sigma.ord       <- cov.aniso(sim.aniso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.aniso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.aniso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
-kls
+# standard vecchia / pilot correlation-based vecchia / standard vecchia with the corrvecchia function / correlation-based vecchia with the corrvecchia function
+# 0.05371653 0.05371653 0.05371653 0.05308003
 
 ####################################################################
-#### example 1 : degree of anisotropy = 10
+#### example 2 : degree of anisotropy = 10
 ####################################################################
 
 locs          <- matrix(runif(15^2 * 2, 0, 1), 15^2, 2)
 n             <- 15^2
-m             <- 15
+m             <- 10
+
 # covmodel
 cov.iso       <- function(locs, covparms) covparms[1] * exp(-fields::rdist(locs) / covparms[2])
 cov.aniso     <- function(locs, covparms) covparms[1] * exp(-fields::rdist(cbind(locs[ ,1] * covparms[3], locs[,2])) / covparms[2])
 covparms      <- c(1, 0.1, 10)
 
+locs.trans    <- cbind(locs[, 1] * covparms[3], locs[, 2])
+
 # true cov matrix
 Sigma <- cov.aniso(locs, covparms)
 
@@ -90,38 +94,35 @@ Sigma <- cov.aniso(locs, covparms)
 y <- as.numeric(t(chol(Sigma)) %*% rnorm(n))
 fields::quilt.plot(locs[,1], locs[,2], y)
 
-sim.iso     <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "euclidean", conditioning = "NN", covmodel = cov.iso, covparms = covparms)
 
-Sigma.ord       <- cov.aniso(sim.iso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.iso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.iso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
+### specify vecchia approximations
+approx <- list()
+
+# standard vecchia
+approx[[1]]           <- vecchia_specify_adjusted(locs, m, ordering = "maxmin", which.coord = NULL, cond.yz='y', conditioning = "NN")
+# pilot correlation-based vecchia
+approx[[2]]           <- vecchia_specify_adjusted(locs.trans, m, ordering = "maxmin", which.coord = NULL, cond.yz='y', conditioning = "NN")
+approx[[2]]$locsord   <- locs[approx[[2]]$ord,]
+# standard vecchia with the corrvecchia function
+approx[[3]]           <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "euclidean", conditioning = "NN", covmodel = cov.iso, covparms = covparms)
+# correlation-based vecchia with the corrvecchia function
+approx[[4]]           <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "correlation", initial.pt = NULL, conditioning = "NN", covmodel = cov.aniso, covparms = covparms)
+
+
+### compute approximate covariance matrices
+Sigma.hat   <- list()
+kls         <- c()
+for(i in 1:4){
+  
+  Sigma.ord       <- cov.aniso(approx[[i]]$locsord, covparms) # true cov in appropriate ordering
+  
+  U               <- createU(approx[[i]], c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
+  revord          <- order(approx[[i]]$ord)
+  Sigma.hat[[i]]  <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
+  
+  kls[i]          <- kldiv(Sigma, Sigma.hat[[i]])
+}
+
 kls
-
-sim.iso     <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "correlation", conditioning = "NN", covmodel = cov.iso, covparms = covparms)
-
-Sigma.ord       <- cov.aniso(sim.iso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.iso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.iso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
-kls
-
-sim.aniso   <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "euclidean", conditioning = "NN", covmodel = cov.aniso, covparms = covparms)
-
-Sigma.ord       <- cov.aniso(sim.aniso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.aniso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.aniso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
-kls
-
-sim.aniso   <- corrvecchia_knownCovparms(locs = locs, m = m, ordering = "maxmin", ordering.method = "correlation", conditioning = "NN", covmodel = cov.aniso, covparms = covparms)
-
-Sigma.ord       <- cov.aniso(sim.aniso$locsord, covparms) # true cov in appropriate ordering
-U               <- createU(sim.aniso, c(1, 0.1, 0.5), 0, covmodel = Sigma.ord)$U
-revord          <- order(sim.aniso$ord)
-Sigma.hat       <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
-kls             <- kldiv(Sigma, Sigma.hat)
-kls
+# standard vecchia / pilot correlation-based vecchia / standard vecchia with the corrvecchia function / correlation-based vecchia with the corrvecchia function
+# 0.4090684634 0.0002972307 0.4090684634 0.0003749438
