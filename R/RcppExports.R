f2c4c1cf593d106984718d27f52909001241304b
Myeongjong <kmj.stat@gmail.com>
2020-03-17 22:23:31 -0500

    [Fix] Fix minor errors in the package building procedure and add short description

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,18 +2,18 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 rcpparma_hello_world <- function() {
-    .Call(`_correlationVecchia_rcpparma_hello_world`)
+    .Call('_correlationVecchia_rcpparma_hello_world', PACKAGE = 'correlationVecchia')
 }
 
 rcpparma_outerproduct <- function(x) {
-    .Call(`_correlationVecchia_rcpparma_outerproduct`, x)
+    .Call('_correlationVecchia_rcpparma_outerproduct', PACKAGE = 'correlationVecchia', x)
 }
 
 rcpparma_innerproduct <- function(x) {
-    .Call(`_correlationVecchia_rcpparma_innerproduct`, x)
+    .Call('_correlationVecchia_rcpparma_innerproduct', PACKAGE = 'correlationVecchia', x)
 }
 
 rcpparma_bothproducts <- function(x) {
-    .Call(`_correlationVecchia_rcpparma_bothproducts`, x)
+    .Call('_correlationVecchia_rcpparma_bothproducts', PACKAGE = 'correlationVecchia', x)
 }
 
