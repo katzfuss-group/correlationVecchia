9538576dcda757c377e078353d1e0e858f1d94b1
myeongjong <kmj.stat@gmail.com>
2021-09-27 02:35:21 -0500

    [Sim] Add a R script to perform simulation for non-euclidean example

---
+++ R/ouput/simulation_noneuc_09272021.R
@@ -0,0 +1,146 @@
+####################################################################################
+###
+###   Author: Myeongjong Kang (kmj.stat@gmail.com)
+###
+###   Overview:
+###
+####################################################################################
+
+rm(list = ls())
+set.seed(09212021)
+
+##
+
+gc()
+
+memory.size() # memory.limit()
+
+##
+
+library(correlationVecchia)
+
+###################################################################
+
+output                <- generate_hnm(mu0 = 0, sig20 = 1, k = 8, mlt = 2)
+
+length(output$y)
+
+covmat                <- covmat_hnm(output)
+cormat                <- cormat_hnm(output)
+
+# fields::imagePlot(cormat)
+
+###################################################################
+
+initial.pt            <- NULL
+ms                    <- c(3, 5, 10, 15, 20, 25, 30, 35, 40, 45)
+
+###################################################################
+
+approx <- list()
+for(i in 1:length(ms)) {
+
+  approx[[i]]           <- cvecchia_specify_noneuc(cormat = cormat, initial.pt = initial.pt, m = ms[i])
+}
+
+n.approx              <- length(approx)
+kls1                  <- rep(NA, n.approx)
+for(i in 1:n.approx) {
+
+  covmat.ord            <- covmat[approx[[i]]$ord, approx[[i]]$ord]
+  U                     <- GPvecchia::createU(approx[[i]], c(1), 0, covmodel = covmat.ord)$U
+
+  revord                <- order(approx[[i]]$ord)
+  covmat.hat            <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
+
+  kls1[i]               <- kldiv(covmat, covmat.hat, method.modify = NULL, pivot = FALSE, tol = .Machine$double.eps)
+}
+
+###################################################################
+
+approx <- list()
+for(i in 1:length(ms)) {
+
+  approx[[i]]           <- lvecchia_specify_noneuc(cormat = cormat, m = ms[i])
+}
+
+n.approx              <- length(approx)
+kls2                  <- rep(NA, n.approx)
+for(i in 1:n.approx) {
+
+  covmat.ord            <- covmat[approx[[i]]$ord, approx[[i]]$ord]
+  U                     <- GPvecchia::createU(approx[[i]], c(1), 0, covmodel = covmat.ord)$U
+
+  revord                <- order(approx[[i]]$ord)
+  covmat.hat            <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
+
+  kls2[i]               <- kldiv(covmat, covmat.hat, method.modify = NULL, pivot = FALSE, tol = .Machine$double.eps)
+}
+
+###################################################################
+
+yrange <- log10(range(c(kls1, kls2)))
+
+plot(ms, log10(kls1), ylim = yrange, type = "o", col = "#E41A1C", pch = 16, lwd = 2, cex = 1.25, xlab = "increasing m", ylab = "log10(KL)", main = "Non-spatial example: Hierarchical normal model")
+lines(ms, log10(kls2), type = "o", col = "#4DAF4A", pch = 17, lwd = 2, cex = 1.25, xlab = "increasing m", ylab = "log10(KL)", main = "Non-spatial example: Hierarchical normal model")
+
+axis(2)
+axis(side = 1, at = ms, labels = ms)
+
+abline(v = ms, lty = 2, col = "black")
+
+###################################################################
+
+nsim <- 200
+
+kls3 <- list()
+for(j in 1:nsim) {
+
+  # approx <- list()
+  # for(i in 1:length(ms)) {
+  #
+  #   approx[[i]]           <- rvecchia_specify_nonE(cormat = cormat, m = ms[i])
+  # }
+
+  approx                <- rvecchias_specify_noneuc(cormat = cormat, ms = ms)
+
+  n.approx              <- length(approx)
+  kls                   <- rep(NA, n.approx)
+  for(i in 1:n.approx) {
+
+    covmat.ord            <- covmat[approx[[i]]$ord, approx[[i]]$ord]
+    U                     <- GPvecchia::createU(approx[[i]], c(1), 0, covmodel = covmat.ord)$U
+
+    revord                <- order(approx[[i]]$ord)
+    covmat.hat            <- as.matrix(solve(Matrix::tcrossprod(U)))[revord,revord]
+
+    kls[i]                <- kldiv(covmat, covmat.hat, method.modify = NULL, pivot = FALSE, tol = .Machine$double.eps)
+  }
+
+  kls3[[j]] <- kls
+}
+
+###################################################################
+
+# png(file = "nonspatial example.png", width = 700, height = 400)
+
+yrange <- log10(range(c(kls1, kls2, unlist(kls3))))
+
+plot(ms, log10(kls1), ylim = yrange, type = "o", col = "#E41A1C", pch = 16, lwd = 2, cex = 1.25, xlab = "increasing m", ylab = "log10(KL)", main = "Non-spatial example: Hierarchical normal model")
+lines(ms, log10(kls2), type = "o", col = "#4DAF4A", pch = 17, lwd = 2, cex = 1.25)
+
+for(j in 1:nsim) lines(ms, log10(kls3[[j]]), type = "o", col = scales::alpha("#377EB8", 0.05), pch = 15, lwd = 2, cex = 1)
+
+axis(2)
+axis(side = 1, at = ms, labels = ms)
+
+abline(v = ms, lty = 2, col = "black")
+
+legend("bottomleft", legend = c("RVecchia", "LVecchia", "CVecchia"), lty = 1, lwd = 2, col = c("#377EB8", "#4DAF4A", "#E41A1C"))
+
+# dev.off()
+
+###################################################################
+
+save(output, cormat, ms, kls1, kls2, kls3, file = "simout_noneuc_09272021.RData")
+
---
+++ R/ouput/simulation_visualization_noneuc.R
@@ -0,0 +1,108 @@
+####################################################################################
+###
+###   Author: Myeongjong Kang (kmj.stat@gmail.com)
+###
+###   Overview:
+###
+####################################################################################
+
+rm(list = ls())
+
+library(dplyr) ; library(ggplot2) ; library(gridExtra) ; library(grid)
+
+####################################################################################
+
+load("simout_noneuc_09272021.RData")
+
+vdat1       <- matrix(NA, nrow = length(ms), ncol = 4 + length(kls3))
+vdat1[, 1]  <- seq(length(ms))
+vdat1[, 2]  <- ms
+vdat1[, 3]  <- kls1
+vdat1[, 4]  <- kls2
+
+for(i in 1:length(kls3)) vdat1[, i + 4] <- kls3[[i]]
+
+vdat1       <- as.data.frame(vdat1)
+colnames(vdat1) <- c("index", "m", paste0("approx_", seq(length(kls3)+2)))
+
+vdat1[1:5, 1:10]
+
+vdat2       <- vdat1[, c("index", "m", "approx_3", "approx_2", "approx_1")]
+
+vdat2
+
+vdat3       <- vdat1 %>% select(index, m, approx_1)
+vdat4       <- vdat1 %>% select(index, m, approx_2)
+vdat5       <- vdat1 %>% select(-approx_1, -approx_2)
+
+vdat1      <- vdat1 %>% select(-index) %>% tidyr::gather(key = "approx", value = "kldiv", -m)
+vdat2      <- vdat2 %>% select(-index) %>% tidyr::gather(key = "approx", value = "kldiv", -m)
+vdat3      <- vdat3 %>% select(-index) %>% tidyr::gather(key = "approx", value = "kldiv", -m)
+vdat4      <- vdat4 %>% select(-index) %>% tidyr::gather(key = "approx", value = "kldiv", -m)
+vdat5      <- vdat5 %>% select(-index) %>% tidyr::gather(key = "approx", value = "kldiv", -m)
+
+####################################################################################
+
+legs        <- c("Random-ord + Random-conditioning", "Lexical-ord + C-NN", "C-MM + C-NN")
+cols        <- c("#377EB8", "#4DAF4A", "#E41A1C")
+shps        <- c(15, 17, 16)
+ftn         <- log10
+alpha       <- 0.7
+size.point  <- 7
+size.tick   <- 1
+size.line   <- 1.4
+size.legend <- 22
+size.lab    <- 22
+size.text   <- 20
+size.margin <- c(5.5, 20, 5.5, 5.5)
+
+####################################################################################
+
+p2 <- vdat2 %>% ggplot(aes(x = m, y = ftn(kldiv), col = approx, shape = approx)) +
+  geom_point(size = size.point) + geom_line(size = size.line, alpha = alpha) +
+  scale_color_manual(values = cols, labels = legs, guide = guide_legend(nrow = 1, byrow = TRUE)) +
+  scale_shape_manual(values = shps, labels = legs, guide = guide_legend(nrow = 1, byrow = TRUE)) +
+  xlab("increasing m") + ylab("log10(KL)") + # coord_cartesian(xlim = xlims[[1]], ylim = ylims[[1]]) +
+  theme(axis.title.x = element_text(size = size.lab),
+        axis.text.x = element_text(size = size.text),
+        axis.title.y = element_text(size = size.lab),
+        axis.text.y = element_text(size = size.text),
+        strip.text = element_text(size = size.lab),
+        legend.title = element_blank(),
+        legend.text = element_text(size = size.legend, margin = margin(r = 25, unit = 'pt')),
+        legend.direction = 'horizontal',
+        legend.spacing.x = unit(15, 'pt'),
+        plot.margin = unit(size.margin, "pt"))
+
+tmp       <- ggplot_gtable(ggplot_build(p2))
+leg       <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+mylegend  <- tmp$grobs[[leg]]
+
+####################################################################################
+
+p1 <- ggplot() +
+  geom_point(data = vdat3, aes(x = m, y = ftn(kldiv)), colour = cols[3], shape = shps[3], size = size.point) +
+  geom_line(data = vdat3, aes(x = m, y = ftn(kldiv)), colour = cols[3], size = size.line, alpha = alpha) +
+  geom_point(data = vdat4, aes(x = m, y = ftn(kldiv)), colour = cols[2], shape = shps[2], size = size.point) +
+  geom_line(data = vdat4, aes(x = m, y = ftn(kldiv)), colour = cols[2], size = size.line, alpha = alpha) +
+  geom_point(data = vdat5, aes(x = m, y = ftn(kldiv), group = approx), colour = cols[1], shape = shps[1], size = size.point) +
+  geom_line(data = vdat5, aes(x = m, y = ftn(kldiv), group = approx), colour = cols[1], size = size.line, alpha = alpha) +
+  xlab("increasing m") + ylab("log10(KL)") + # coord_cartesian(xlim = xlims[[1]], ylim = ylims[[1]]) +
+  theme(axis.title.x = element_text(size = size.lab),
+        axis.text.x = element_text(size = size.text),
+        axis.title.y = element_text(size = size.lab),
+        axis.text.y = element_text(size = size.text),
+        strip.text = element_text(size = size.lab),
+        legend.title = element_blank(),
+        legend.text = element_text(size = size.legend, margin = margin(r = 25, unit = 'pt')),
+        legend.direction = 'horizontal',
+        legend.spacing.x = unit(15, 'pt'),
+        plot.margin = unit(size.margin, "pt"))
+
+####################################################################################
+
+result    <- grid.arrange(mylegend, p1 + theme(legend.position="none"), nrow=2, heights=c(1, 10))
+
+ggplot2::ggsave("simulation_noneuc.pdf", result, width = 15.2, height = 10)
+
+####################################################################################
